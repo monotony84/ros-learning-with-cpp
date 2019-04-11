e28a2033f5baf2def517c8afed5d9019dc32072c
monotony <monotony84.d@gmail.com>
2019-04-11 23:01:16 +0900

    modify variable name

--- ros_tutorials_action/src/action_server.cpp
+++ ros_tutorials_action/src/action_server.cpp
@@ -4,18 +4,18 @@
 
 class FibonacciAction {
  protected:
-  ros::NodeHandle nh;
+  ros::NodeHandle nh_;
 
-  actionlib::SimpleActionServer<ros_tutorials_action::FibonacciAction> action_server;
-  std::string action_name;
+  actionlib::SimpleActionServer<ros_tutorials_action::FibonacciAction> action_server_;
+  std::string action_name_;
 
-  ros_tutorials_action::FibonacciFeedback feedback;
-  ros_tutorials_action::FibonacciResult result;
+  ros_tutorials_action::FibonacciFeedback feedback_;
+  ros_tutorials_action::FibonacciResult result_;
 
  public:
   FibonacciAction(std::string name) :
-      action_server(nh, name, boost::bind(&FibonacciAction::executeCB, this, _1), false), action_name(name) {
-    action_server.start();
+      action_server_(nh_, name, boost::bind(&FibonacciAction::executeCB, this, _1), false), action_name_(name) {
+    action_server_.start();
   }
 
   ~FibonacciAction(void) {
@@ -26,36 +26,36 @@ class FibonacciAction {
     ros::Rate r(1);
     bool success = true;
 
-    feedback.sequence.clear();
-    feedback.sequence.push_back(0);
-    feedback.sequence.push_back(1);
+    feedback_.sequence.clear();
+    feedback_.sequence.push_back(0);
+    feedback_.sequence.push_back(1);
 
     ROS_INFO("%s: Executing, creating fibonacci sequence of order %i with seeds %i, %i",
-             action_name.c_str(), goal->order, feedback.sequence[0], feedback.sequence[1]);
+             action_name_.c_str(), goal->order, feedback_.sequence[0], feedback_.sequence[1]);
 
     for (int i = 1; i <= goal->order; i++) {
-      if (action_server.isPreemptRequested() || !ros::ok()) {
-        ROS_INFO("%s: Preempted", action_name.c_str());
-        action_server.setPreempted();
+      if (action_server_.isPreemptRequested() || !ros::ok()) {
+        ROS_INFO("%s: Preempted", action_name_.c_str());
+        action_server_.setPreempted();
         success = false;
         break;
       }
-      feedback.sequence.push_back(feedback.sequence[i] + feedback.sequence[i - 1]);
-      action_server.publishFeedback(feedback);
+      feedback_.sequence.push_back(feedback_.sequence[i] + feedback_.sequence[i - 1]);
+      action_server_.publishFeedback(feedback_);
       r.sleep();
     }
 
     if (success) {
-      result.sequence = feedback.sequence;
-      ROS_INFO("%s: Succeeded", action_name.c_str());
-      action_server.setSucceeded(result);
+      result_.sequence = feedback_.sequence;
+      ROS_INFO("%s: Succeeded", action_name_.c_str());
+      action_server_.setSucceeded(result_);
     }
   }
 
 };
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "action_server");
+  ros::init(argc, argv, "action_server_");
   FibonacciAction fibonacci("ros_tutorial_action");
 
   ros::spin();
