183e82ec5a84a11809a800c2056e093fdf12a614
monotony <monotony84.d@gmail.com>
2019-04-11 23:23:39 +0900

    add multi culculation_method

--- ros_tutorials_service/src/service_server.cpp
+++ ros_tutorials_service/src/service_server.cpp
@@ -1,10 +1,39 @@
 #include "ros/ros.h"
 #include "ros_tutorials_service/SrvTutorial.h"
 
+#define PLUS            1
+#define MINUS           2
+#define MULTIPLICATION  3
+#define DIVISION        4
+
+int g_operator = PLUS;
+
 // when a service request is received, it execute below.
 bool calculation(ros_tutorials_service::SrvTutorial::Request &req,
                  ros_tutorials_service::SrvTutorial::Response &res) {
-  res.result = req.a + req.b;
+
+  switch (g_operator){
+    case PLUS:
+      res.result = req.a + req.b;
+      break;
+    case MINUS:
+      res.result = req.a - req.b;
+      break;
+    case MULTIPLICATION:
+      res.result = req.a * req.b;
+      break;
+    case DIVISION:
+      if(req.b == 0) {
+        res.result = 0;
+        break;
+      } else {
+        res.result = req.a / req.b;
+        break;
+      }
+    default:
+      res.result = req.a + req.b;
+      break;
+  }
 
   ROS_INFO("request x=%ld y=%ld", (long int) req.a, (long int) req.b);
   ROS_INFO("sending back response: %ld", (long int) res.result);
@@ -13,15 +42,20 @@ bool calculation(ros_tutorials_service::SrvTutorial::Request &req,
 }
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "topic_Subscriber");
+  ros::init(argc, argv, "service_server");
   ros::NodeHandle nh;
+
+  nh.setParam("calculation_method", PLUS);
 
   ros::ServiceServer ros_tutorials_service_server = nh.advertiseService("ros_tutorial_srv", calculation);
 
   ROS_INFO("ready srv server!");
 
-  // wait for service request.
-  ros::spin();
-
+  ros::Rate r(10); //Hz
+  while(1) {
+    nh.getParam("calculation_method", g_operator);
+    ros::spinOnce(); //wait for service request.
+    r.sleep();
+  }
   return 0;
 }
